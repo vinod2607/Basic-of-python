8efe2712d113774ecd908790bfe5a0e4271cc42b
vinod2607 <shettyvinod317@gmail.com>
2024-11-04 17:12:46 +0530

    add list

---
+++ python/1.py
@@ -0,0 +1,13 @@
+num1 = ('enter the first number = ')
+num2 = ('enter the second number = ')
+
+
+
+
+ num1 = float(input("Enter the first number: "))
+ num2 = float(input("Enter the second number: "))
+ print("Please enter valid numbers.")
+
+
+
+
---
+++ python/2.py
@@ -0,0 +1,33 @@
+#lists
+
+items = ["bru", "sugar" , "milk" , "biscuit"]
+print(items)
+
+items.pop()
+print(items)
+
+items.pop(0)
+print(items)
+
+
+items.append("parle")
+print(items)
+
+items.remove("sugar")
+print(items)
+
+items.insert(2,"spoon")
+print(items)
+
+items[0] = "coffe powder"
+print(items)
+
+#slicing
+
+l = [100, 200, 300, 4000, 500]
+l2 = l[1:4]
+print(l2)
+
+
+M = [[2,4] , [6,9]]
+print(type(M))
